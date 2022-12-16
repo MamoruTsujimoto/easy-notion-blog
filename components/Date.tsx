33074913db549cb1830ebcac850680611c1f0503
TsujimotoSirChristopherMamoru <tsujimotosirchristophermamoru@gmail.com>
2022-12-16 16:31:56 +0900

    Fix Minified React error

--- components/Date.tsx
+++ components/Date.tsx
@@ -1,12 +1,13 @@
-import { parseISO, format } from 'date-fns'
+//import { parseISO, format } from 'date-fns'
 
 type Props = {
   dateString: string
 }
 
 const Date = ({ dateString }: Props) => {
-  const date = parseISO(dateString)
-  return <span>{format(date, 'LLLL	d, yyyy')}</span>
+  //const date = parseISO(dateString)
+  console.log(dateString)
+  return <span>test</span>
 }
 
 export default Date
