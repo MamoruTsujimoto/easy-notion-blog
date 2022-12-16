d8983f08ece17379eff6cbc7778141e8d16fc479
TsujimotoSirChristopherMamoru <tsujimotosirchristophermamoru@gmail.com>
2022-12-16 15:50:19 +0900

    check Minified React error

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,16 @@
 import { NUMBER_OF_POSTS_PER_PAGE } from 'app/server-constants'
-import { NextPageLink, NoContents } from 'components/blog-parts'
+import { NoContents } from 'components/blog-parts'
 import Wrapper from 'components/base/Wrapper'
-import CardLarge from 'components/card/CardLarge'
-import CardSmall from 'components/card/CardSmall'
-import config from 'utils/config'
 import { getPosts, getFirstPost } from 'lib/notion/client'
 
 export const revalidate = 60
 
 const RootPage = async () => {
-  const [posts, firstPost] = await Promise.all([getPosts(NUMBER_OF_POSTS_PER_PAGE), getFirstPost()])
-  const postNum = config.setting.postNum
-  const newPost = posts[0]
-  const pastPost = posts.slice(1, postNum)
+  const [posts] = await Promise.all([getPosts(NUMBER_OF_POSTS_PER_PAGE), getFirstPost()])
 
   return (
     <Wrapper>
       <NoContents contents={posts} />
-      <CardLarge post={newPost} />
-      <CardSmall title='archives' posts={pastPost} />
-
-      <footer>
-        <NextPageLink firstPost={firstPost} posts={posts} />
-      </footer>
     </Wrapper>
   )
 }
--- components/card/CardLarge.tsx
+++ components/card/CardLarge.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from 'next/link'
 import Image from 'next/image'
 import styled from '@emotion/styled'
 import { Post } from 'lib/notion/interfaces'
@@ -25,36 +26,40 @@ const CardLarge = ({ post }: Props) => {
   return (
     <CMSection>
       <CMArticle id={post.Date}>
-        <CMBody>
-          <ul>
-            <li>{post.Tags && post.Tags.length > 0 && post.Tags.map((tag: string) => <span key={tag}>{tag}</span>)}</li>
-          </ul>
-          <h1>{post.Title}</h1>
-          <CMInformation>
-            <ul className='story-status'>
+        <Link as={`/blog/${post.Slug}`} href={`/blog/${post.Slug}`} passHref scroll={false}>
+          <CMBody>
+            <ul>
               <li>
-                <span className='story-publish'>
-                  <DateFormatter dateString={post.Date} />
-                </span>
+                {post.Tags && post.Tags.length > 0 && post.Tags.map((tag: string) => <span key={tag}>{tag}</span>)}
               </li>
             </ul>
-            <p>{post.Excerpt}</p>
-          </CMInformation>
-          <ButtonLarge>read more</ButtonLarge>
-        </CMBody>
-        <CMPicture>
-          <div className='new-image outline'>
-            <Image
-              src={imageURL}
-              width={780}
-              height={580}
-              alt={post.coverCaption}
-              onLoad={onLoad}
-              priority={true}
-              style={{ objectFit: 'cover' }}
-            />
-          </div>
-        </CMPicture>
+            <h1>{post.Title}</h1>
+            <CMInformation>
+              <ul className='story-status'>
+                <li>
+                  <span className='story-publish'>
+                    <DateFormatter dateString={post.Date} />
+                  </span>
+                </li>
+              </ul>
+              <p>{post.Excerpt}</p>
+            </CMInformation>
+            <ButtonLarge>read more</ButtonLarge>
+          </CMBody>
+          <CMPicture>
+            <div className='new-image outline'>
+              <Image
+                src={imageURL}
+                width={780}
+                height={580}
+                alt={post.coverCaption}
+                onLoad={onLoad}
+                priority={true}
+                style={{ objectFit: 'cover' }}
+              />
+            </div>
+          </CMPicture>
+        </Link>
       </CMArticle>
     </CMSection>
   )
