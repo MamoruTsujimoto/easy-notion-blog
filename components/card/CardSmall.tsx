e6095ce206c60a1fc732a0ddb36d4002e1c84097
TsujimotoSirChristopherMamoru <tsujimotosirchristophermamoru@gmail.com>
2022-12-16 16:04:18 +0900

    check Minified React error

--- components/card/CardSmall.tsx
+++ components/card/CardSmall.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import Link from 'next/link'
-import Image from 'next/image'
+// import Link from 'next/link'
+// import Image from 'next/image'
 import styled from '@emotion/styled'
 import { Post } from 'lib/notion/interfaces'
-import DateFormatter from 'components/Date'
+//import DateFormatter from 'components/Date'
 import styles from 'utils/styles'
 
-import { NEXT_PUBLIC_URL } from 'app/server-constants'
+//import { NEXT_PUBLIC_URL } from 'app/server-constants'
 
 type Props = {
   posts: Post[]
@@ -15,47 +15,16 @@ type Props = {
 }
 
 const CardSmall = ({ posts, title }: Props) => {
-  const onLoad = (e) => {
-    if (e.target.srcset) {
-      e.target.dataset.load = 'done'
-    }
-  }
-
+  // const onLoad = (e) => {
+  //   if (e.target.srcset) {
+  //     e.target.dataset.load = 'done'
+  //   }
+  // }
+  console.log(posts)
   return (
     <Section>
       <h1 className='section-title'>{title}</h1>
-      <ArticleWrapper>
-        {posts.map((post) => (
-          <Article key={post.Slug}>
-            <Link as={`/blog/${post.Slug}`} href={`/blog/${post.Slug}`} passHref scroll={false}>
-              <div className='story-figure figure'>
-                <Image
-                  src={new URL(`/api/eye-catch/${post.Slug}`, NEXT_PUBLIC_URL).toString()}
-                  width={373}
-                  height={250}
-                  alt={post.coverCaption}
-                  onLoad={onLoad}
-                  style={{ objectFit: 'cover' }}
-                />
-              </div>
-              <div className='story-entrance'>
-                <span className='story-category'>{post.Tags}</span>
-                <h1>{post.Title}</h1>
-                <div className='story-information'>
-                  <ul className='story-status'>
-                    <li>
-                      <span className='story-publish'>
-                        <DateFormatter dateString={post.Date} />
-                      </span>
-                    </li>
-                  </ul>
-                  <p>{post.Excerpt}</p>
-                </div>
-              </div>
-            </Link>
-          </Article>
-        ))}
-      </ArticleWrapper>
+      <ArticleWrapper></ArticleWrapper>
     </Section>
   )
 }
@@ -257,185 +226,185 @@ const ArticleWrapper = styled.div`
     grid-template-columns: repeat(1, 1fr);
   }
 `
-const Article = styled.article`
-  .story {
-    &-past {
-      // opacity: 0;
-      // transform: translateY(50px);
-      // transition-duration: .6s;
+// const Article = styled.article`
+//   .story {
+//     &-past {
+//       // opacity: 0;
+//       // transform: translateY(50px);
+//       // transition-duration: .6s;
 
-      article {
-        position: relative;
-        width: 100%;
-        min-height: 825px;
+//       article {
+//         position: relative;
+//         width: 100%;
+//         min-height: 825px;
 
-        @media (max-width: ${styles.sizes.breakpoint.small}) {
-          margin-bottom: 35px;
-          &:after {
-            content: '⋯';
-            display: block;
-            text-align: center;
-            color: #333;
-          }
-        }
+//         @media (max-width: ${styles.sizes.breakpoint.small}) {
+//           margin-bottom: 35px;
+//           &:after {
+//             content: '⋯';
+//             display: block;
+//             text-align: center;
+//             color: #333;
+//           }
+//         }
 
-        &:last-of-type {
-          margin-right: 0;
+//         &:last-of-type {
+//           margin-right: 0;
 
-          @media (max-width: ${styles.sizes.breakpoint.small}) {
-            margin-bottom: 0;
+//           @media (max-width: ${styles.sizes.breakpoint.small}) {
+//             margin-bottom: 0;
 
-            &:after {
-              content: '';
-            }
-          }
-        }
+//             &:after {
+//               content: '';
+//             }
+//           }
+//         }
 
-        h1 {
-          position: relative;
-          margin: 10px 0 50px;
-          min-height: 80px;
-          font-family: 'ヒラギノ明朝 ProN W3', HiraMinProN-W3, YuMincho, 游明朝, 'ＭＳ Ｐ明朝', 'MS PMincho', 'MS 明朝',
-            serif;
+//         h1 {
+//           position: relative;
+//           margin: 10px 0 50px;
+//           min-height: 80px;
+//           font-family: 'ヒラギノ明朝 ProN W3', HiraMinProN-W3, YuMincho, 游明朝, 'ＭＳ Ｐ明朝', 'MS PMincho', 'MS 明朝',
+//             serif;
 
-          ${styles.mixins.fontSize(24, 40)}
+//           ${styles.mixins.fontSize(24, 40)}
 
-          letter-spacing: 0.15em;
+//           letter-spacing: 0.15em;
 
-          @media (max-width: ${styles.sizes.breakpoint.small}) {
-            min-height: auto;
-            max-height: auto;
-          }
+//           @media (max-width: ${styles.sizes.breakpoint.small}) {
+//             min-height: auto;
+//             max-height: auto;
+//           }
 
-          &:before {
-            content: '';
-            position: absolute;
-            left: 0;
-            bottom: -15px;
-            width: 30px;
-            height: 2px;
-            background-color: #464646;
-            z-index: 2;
-            transition: all 200ms 0s ease;
-          }
+//           &:before {
+//             content: '';
+//             position: absolute;
+//             left: 0;
+//             bottom: -15px;
+//             width: 30px;
+//             height: 2px;
+//             background-color: #464646;
+//             z-index: 2;
+//             transition: all 200ms 0s ease;
+//           }
 
-          &:after {
-            content: '';
-            position: absolute;
-            left: 0;
-            bottom: -15px;
-            width: 100px;
-            height: 2px;
-            background-color: #ddd;
-          }
-        }
+//           &:after {
+//             content: '';
+//             position: absolute;
+//             left: 0;
+//             bottom: -15px;
+//             width: 100px;
+//             height: 2px;
+//             background-color: #ddd;
+//           }
+//         }
 
-        a {
-          display: block;
-          color: #333;
+//         a {
+//           display: block;
+//           color: #333;
 
-          &:hover {
-            h1 {
-              &:before {
-                width: 100px;
-              }
-            }
-          }
-          .story-figure {
-            &:hover {
-              opacity: 1;
-              filter: grayscale(0);
-            }
-          }
-        }
+//           &:hover {
+//             h1 {
+//               &:before {
+//                 width: 100px;
+//               }
+//             }
+//           }
+//           .story-figure {
+//             &:hover {
+//               opacity: 1;
+//               filter: grayscale(0);
+//             }
+//           }
+//         }
 
-        .story-publish {
-          color: #4b4b4b;
-        }
-      }
+//         .story-publish {
+//           color: #4b4b4b;
+//         }
+//       }
 
-      &.active {
-        opacity: 1;
-        transform: translate(0, 0);
-      }
-    }
+//       &.active {
+//         opacity: 1;
+//         transform: translate(0, 0);
+//       }
+//     }
 
-    &-category {
-      ${styles.mixins.fontSize(12, 12)}
+//     &-category {
+//       ${styles.mixins.fontSize(12, 12)}
 
-      font-family: ${styles.fonts.raleway};
+//       font-family: ${styles.fonts.raleway};
 
-      color: #4b4b4b;
-    }
+//       color: #4b4b4b;
+//     }
 
-    &-status {
-      li {
-        text-align: right;
-      }
-    }
+//     &-status {
+//       li {
+//         text-align: right;
+//       }
+//     }
 
-    &-entrance {
-      padding: 13px;
-    }
+//     &-entrance {
+//       padding: 13px;
+//     }
 
-    &-information {
-      width: 355px;
-      width: 100%;
-      margin: auto;
+//     &-information {
+//       width: 355px;
+//       width: 100%;
+//       margin: auto;
 
-      p {
-        ${styles.mixins.fontSize(14, 22)}
+//       p {
+//         ${styles.mixins.fontSize(14, 22)}
 
-        color: #333;
+//         color: #333;
 
-        &:first-letter {
-          ${styles.mixins.fontSize(20, 22)}
+//         &:first-letter {
+//           ${styles.mixins.fontSize(20, 22)}
 
-          color: #111;
-        }
-      }
-    }
+//           color: #111;
+//         }
+//       }
+//     }
 
-    &-publish {
-      ${styles.mixins.fontSize(11, 11)}
+//     &-publish {
+//       ${styles.mixins.fontSize(11, 11)}
 
-      color: #b8b8b8;
-    }
+//       color: #b8b8b8;
+//     }
 
-    &-figure {
-      width: 100%;
-      height: 250px;
-      transition: all 1000ms 0s ease;
-      filter: grayscale(100%);
+//     &-figure {
+//       width: 100%;
+//       height: 250px;
+//       transition: all 1000ms 0s ease;
+//       filter: grayscale(100%);
 
-      @media (max-width: ${styles.sizes.breakpoint.small}) {
-        padding-top: 0;
-        filter: grayscale(0);
-      }
+//       @media (max-width: ${styles.sizes.breakpoint.small}) {
+//         padding-top: 0;
+//         filter: grayscale(0);
+//       }
 
-      &.other {
-        background-position: center center;
-        background-size: 100%;
-      }
+//       &.other {
+//         background-position: center center;
+//         background-size: 100%;
+//       }
 
-      &.outline {
-        border: 1px solid #dddddd;
+//       &.outline {
+//         border: 1px solid #dddddd;
 
-        @media (max-width: ${styles.sizes.breakpoint.small}) {
-          border: none;
-          border-top: 1px solid $color-border;
-        }
-      }
+//         @media (max-width: ${styles.sizes.breakpoint.small}) {
+//           border: none;
+//           border-top: 1px solid $color-border;
+//         }
+//       }
 
-      &.right {
-        background-position: center right;
-      }
+//       &.right {
+//         background-position: center right;
+//       }
 
-      &.left {
-        background-position: center left;
-      }
-    }
-  }
-`
+//       &.left {
+//         background-position: center left;
+//       }
+//     }
+//   }
+// `
 
 export default CardSmall
