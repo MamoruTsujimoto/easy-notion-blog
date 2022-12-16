71943f45c05e14f61d033cee3462f072f1ca6ed9
TsujimotoSirChristopherMamoru <tsujimotosirchristophermamoru@gmail.com>
2022-12-16 18:43:55 +0900

    fix: Image props

--- components/card/CardSmall.tsx
+++ components/card/CardSmall.tsx
@@ -14,23 +14,6 @@ type Props = {
   title: string
 }
 
-const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`
-
-const toBase64 = (str: string) =>
-  typeof window === 'undefined' ? Buffer.from(str).toString('base64') : window.btoa(str)
-
 const CardSmall = ({ posts, title }: Props) => {
   const onLoad = (e) => {
     if (e.target.srcset) {
@@ -51,8 +34,6 @@ const CardSmall = ({ posts, title }: Props) => {
                   width={373}
                   height={250}
                   alt={post.coverCaption}
-                  placeholder='blur'
-                  blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
                   onLoad={onLoad}
                   style={{ objectFit: 'cover' }}
                 />
